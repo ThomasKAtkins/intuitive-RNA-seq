823fc04e5521dff9c60c10e251abcdcf05daafde
atkin423 <atkin423@umn.edu>
2021-06-25 17:10:31 -0500

    added function code skeleton

---
+++ functions.R
@@ -0,0 +1,29 @@
+#generates Gene interaction network from raw counts
+
+calculate_gin <- function(counts) {
+  
+}
+
+#plots given gene(s) over time (counts vs. time) (control if desired)
+
+plot_genes <- function(counts, genes, control=F) {
+  
+}
+
+#calculates correlations of two genes
+
+corr <- function(gene1, gene2) {
+  
+}
+
+#returns list of most changed genes
+
+most_changed <- function(counts) {
+  
+}
+
+#given graph, calculate clustering coefficient
+
+clustering_coefficient <- function(gin, gene) {
+  
+}
